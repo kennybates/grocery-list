3f5323b585df1d125fbdafdf37b04307fa949cc2
Kenny Bates <kennyleebates@outlook.com>
2021-10-20 15:55:46 -0400

    Update App.js
    pass removeAlert prop in alert to handle remove alert in Alert.js

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
   return (
     <section className='section-center'>
       <form  className='grocery-form' onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert}/>}
+        {alert.show && <Alert {...alert} removeAlert={showAlert}/>}
         <h3> Grocery List</h3>
         <div className='form-control'>
           <input type='text' className='grocery' placeholder='e.g. eggs' value={name} onChange={(e)=> setName(e.target.value)}>
