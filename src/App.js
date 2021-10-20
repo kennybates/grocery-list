20b22aa7164e20495a2c3ca2fc5767a13bd54979
Kenny Bates <kennyleebates@outlook.com>
2021-10-20 14:09:38 -0400

    Update App.js
    Created state values.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import List from './List';
 import Alert from './Alert';
 
 function App() {
+  // State Values
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null); // which ID will be edited
+  const [alert, setAlert] = useState({ show: false, msg: '', type: ''});
+
   return (
     <article>
       <div className='container'>
