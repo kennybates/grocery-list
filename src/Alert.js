d66022a7686f67c119d85db51c4d2def390d70f8
Kenny Bates <kennyleebates@outlook.com>
2021-10-20 15:54:52 -0400

    Update Alert.js
    create useEffect callback to removeAlert after 3 seconds.

--- src/Alert.js
+++ src/Alert.js
@@ -1,9 +1,19 @@
 import React, { useEffect } from 'react';
 
-const Alert = ({ msg, type }) => {
+const Alert = ({ msg, type, removeAlert }) => {
+
+    //useEffect callback when component renders
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            removeAlert(); 
+        }, 3000); // removeAlert() - after 3 seconds
+        return () => clearTimeout(timeout); // callback clear timeout
+    }, []);
     return (
         <p className={`alert alert-${type}`}>{msg}</p>
     );
 };
+
+
 
 export default Alert;
